32850f7ff3a25ccd70b3e11681c9a85cbe8675a9
0kaa <thebestoka0@gmail.com>
2023-02-19 08:15:01 +0200

    🚀 RELEASE: one

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ $(
             nav: true,
             loop: false,
             rtl: true,
+            dots: false,
             navText: ['<i class="fa fa-arrow-left"></i>', '<i class="fa fa-arrow-right"></i>'],
             responsive: {
                 0: {
                     nav: false,
+                    dots: true
                 },
-                800: {
+                1024: {
                     nav: true,
-                    dots: false
                 },
             }
         });
@@ -22,11 +23,13 @@ $(
             nav: false,
             loop: true,
             rtl: true,
+            dots: false,
             margin: 20,
             center: true,
             responsive: {
                 0: {
                     items: 1,
+                    dots: true,
                 },
                 800: {
                     items: 2,
@@ -37,17 +40,44 @@ $(
         $('#section-correspondence').owlCarousel({
             items: 3,
             rtl: true,
-            // dots: true,
+            dots: false,
+            loop: true,
             margin: 10,
             responsive: {
                 0: {
                     items: 1,
+                    dots: true,
                 },
                 600: {
                     items: 2,
                 },
                 1000: {
                     items: 3,
+
+                }
+            }
+
+        })
+
+        $('#circles-section').owlCarousel({
+            items: 3,
+            rtl: true,
+            loop: true,
+            dots: false,
+            margin: 20,
+            responsive: {
+                0: {
+                    items: 1,
+                    dots: true,
+                    center: true,
+                    margin: 0,
+                },
+                600: {
+                    items: 2,
+                    dots: true,
+                },
+                1350: {
+                    items: 3
                 }
             }
 
